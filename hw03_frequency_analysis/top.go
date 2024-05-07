ca0524a8b806e9c4780d031434d000b86c68d6f1
Юрий Астахов <uranus@xaker.ru>
2024-05-07 05:29:24 +0300

    HW3 implemented Top10 function

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,35 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"slices"
+	"strings"
+)
+
+func Top10(s string) []string {
+	words := map[string]int{}
+	for _, word := range strings.Fields(s) {
+		words[word]++
+	}
+
+	keys := make([]string, 0, len(words))
+	for key := range words {
+		keys = append(keys, key)
+	}
+
+	slices.SortFunc(keys, func(a, b string) int {
+		result := words[b] - words[a]
+		if result == 0 {
+			result = strings.Compare(a, b)
+		}
+		return result
+	})
+
+	return keys[:min(10, len(keys))]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
