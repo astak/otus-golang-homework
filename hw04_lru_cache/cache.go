390a10ae93bed1fa71a66eeb56a7cd33dd319dde
Юрий Астахов <uranus@xaker.ru>
2024-05-11 08:34:58 +0300

    HW04: implemented a cache based on a previously written list

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,14 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+type cacheItem struct {
+	key   Key
+	value interface{}
 }
 
 func NewCache(capacity int) Cache {
@@ -22,4 +25,57 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	qItem, ok := cache.items[key]
+	if !ok {
+		return nil, false
+	}
+
+	cache.queue.MoveToFront(qItem)
+
+	return getCacheItem(qItem).value, true
+}
+
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	qItem, ok := cache.items[key]
+	if ok {
+		qItem.Value = cacheItem{
+			key:   key,
+			value: value,
+		}
+		cache.queue.MoveToFront(qItem)
+		return true
+	}
+
+	cItem := cacheItem{
+		key:   key,
+		value: value,
+	}
+	qItem = cache.queue.PushFront(cItem)
+	cache.items[key] = qItem
+
+	if cache.queue.Len() > cache.capacity {
+		qLast := cache.queue.Back()
+		cLast := getCacheItem(qLast)
+		delete(cache.items, cLast.key)
+		cache.queue.Remove(qLast)
+	}
+
+	return false
+}
+
+func (cache *lruCache) Clear() {
+	cache.items = make(map[Key]*ListItem, cache.capacity)
+	cache.queue = NewList()
+}
+
+func getCacheItem(queueItem *ListItem) cacheItem {
+	cacheItem, ok := queueItem.Value.(cacheItem)
+	if !ok {
+		panic("unexpected value type")
+	}
+
+	return cacheItem
 }
